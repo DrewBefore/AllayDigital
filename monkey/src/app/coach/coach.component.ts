8674a779aef4c6b8e1ded16df973843c2df44710
dbefore@uw.edu <andrew.before@HCL.COM>
2017-11-13 16:14:09 -0800

    team hotfix

--- monkey/src/app/coach/coach.component.ts
+++ monkey/src/app/coach/coach.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit, Input} from '@angular/core';
   styleUrls: ['./coach.component.css']
 })
 export class CoachComponent implements OnInit {
-  @Input() member: object;
+  @Input() member;
 
   constructor() {
+    this.member = {name: "", bio: [""], img: "", title: ""};
 }
 
 
